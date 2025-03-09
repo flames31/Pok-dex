74b04d30d17a136ad2b47103a21bbc0a9e8f7f22
flames31 <rrahulao2000@gmail.com>
2025-03-09 13:15:08 -0400

    Added cmd line repl to main

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	for {
+		fmt.Print("Pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) > 0 {
+			fmt.Println("Your command was: " + words[0])
+		}
+	}
 }
